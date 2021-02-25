187eff33f14a8dc05af437e80da0ff73692b46a1
sMarthz <72910166+sMarthz@users.noreply.github.com>
2021-02-25 21:40:27 +0100

    logo + imagenes

--- AngularCode/src/app/app.module.ts
+++ AngularCode/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { Ng2CarouselamosModule } from 'ng2-carouselamos';
-import { GaleriaComponent } from './galeria/galeria.component';    //Este es el import del paquete
+import { GaleriaComponent } from './galeria/galeria.component';
+import { Pagina404Component } from './pagina404/pagina404.component';    //Este es el import del paquete
+
+const rutas = [
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: '**', component: Pagina404Component}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    GaleriaComponent
+    GaleriaComponent,
+    Pagina404Component
   ],
   imports: [
     BrowserModule,
-    Ng2CarouselamosModule                                   //esto se añade tambien
+    Ng2CarouselamosModule,                                  //esto se añade tambien
+    RouterModule.forRoot(rutas) 
   ],
   providers: [],
   bootstrap: [AppComponent]
